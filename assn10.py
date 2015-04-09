6eeca444857717ceb6bba4c2a304beb781b75265
unknown <david-shelton@ws3260.RiversideHealthCare.net>
2015-04-09 11:41:33 -0500

    Assignment 10 files

---
+++ assn10.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+"""
+ITS 3210 Introduction to Scripting Languages
+Governor's State University
+Student Name
+
+Assignment 10
+
+In this assignment, you will read the contents of a custom data file into a CSV file. You
+will need to write custom code to parse this data type and determine appropriate column
+headers on your own. You must identify the pattern and then parse the file. You may use
+advanced string handling techniques, regular expressions, and any modules deemed helpful
+to accomplish this task.
+
+You will be parsing the included 'proprietary_data.txt' file, which contains data that
+is structured in a proprietary way that you must determine. Put the first and last names
+of each person in the file in separate columns in the CSV file. Do not put the numbers
+at the beginning of each line in the CSV file nor any asterisks, only write the relevant 
+data in each column.
+"""
+
+import csv
+import itertools
+from itertools import islice
+from sys import argv
+import string
+#import prop_data.csv
+
+infile = argv[1]
+textfile = open('proprietary_data.txt', 'r')
+
+headers = ['Serial', 'Make', 'Model', 'First', 'Last']
+lines = []
+
+for line in textfile:
+	line = line[5:]
+	line = line.strip()
+	line = line.strip('*')
+	line = line.strip(' ')
+	lines.append(line)
+
+def chunk(it, size):
+    it = iter(it)
+    return iter(lambda: tuple(islice(it, size)), ())
+
+grouped = itertools.izip([lines]*5)
+
+
+	
+with open(infile, 'w') as csvfile:
+	writer = csv.writer(csvfile)
+	writer.writerow(headers)
+	writer.writerows(chunk(lines, 4))
+print lines
+
+	
+raw_input()
